e0f07898122cc89ba0accd4cd27d1dbd1808ac16
Ali Allam <ali.w.allam74@gmail.com>
2026-06-09 22:49:09 +0300

    Update data_explorer.py

--- pages/data_explorer.py
+++ pages/data_explorer.py
@@ -10,7 +10,7 @@ def local_css(file_name):
         with open(file_name, "r") as f:
             st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("d:/Depi Project/style.css")
+local_css("style.css")
 
 if 'filtered_df' not in st.session_state:
     st.warning("Please go to the Home page first to initialize data.")
